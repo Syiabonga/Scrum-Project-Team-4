7f3ea6517c5ba46d9537d2f121e73c15a1b7a57d
leandro Syiabonga <syiabonga111@gmail.com>
2022-11-10 00:31:02 -0600

    update time from 6 to 60

--- TowerDefense_TheRPG/TowerDefense_TheRPG/FrmMain.cs
+++ TowerDefense_TheRPG/TowerDefense_TheRPG/FrmMain.cs
@@ -61,7 +61,7 @@ namespace TowerDefense_TheRPG
             //generate boss balloon every 60 minute
             GenEnemyPos(out int x, out int y);
             Enemy bossBalloon;
-            if (counter % 6 == 0)
+            if (counter % 60 == 0)
             {
                 bossBalloon = Enemy.MakeBossBalloon(x, y);
                 enemies.Add(bossBalloon);
